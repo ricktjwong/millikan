770dcc6063b76ca7a3b46c55f1bec4c6a3b40593
Rick Wong <ricktjwong@Ricks-MacBook-Pro.local>
2018-02-02 23:25:08 +0000

    no message

--- processing_automate.py
+++ processing_automate.py
@@ -7,18 +7,26 @@ Created on Fri Feb  2 21:09:54 2018
 """
 
 import pandas as pd
-import matplotlib.pyplot as plt
+import operator
 import os
+import numpy as np
 
-def returnParticles():
+def returnParticles(middle):
     filtered = []
     turning_indices = []
     for i in pivoted_y:
         turning_index = pivoted_y[i].idxmax()
-        if abs(turning_index - len(pivoted_y.index())/2) < 20:
+        if abs(turning_index - middle) < 20:
             filtered.append(i)
             turning_indices.append(turning_index)
     return filtered, turning_indices
+
+def getIndices():
+    turning_indices = []
+    for i in pivoted_y:
+        turning_index = pivoted_y[i].idxmax()
+        turning_indices.append(turning_index)
+    return turning_indices
 
 def returnFinal(voltage):
     final = []
@@ -26,8 +34,12 @@ def returnFinal(voltage):
         particle = pivoted_dy[i]
         bef = particle[:j-pivoted_dy.index[0]]
         aft = particle[j-pivoted_dy.index[0]:]
-        v_e = bef.sum()/len(bef)
-        v_g = aft.sum()/len(aft)
+        if len(bef) > 0 and len(aft) > 0:
+            v_e = bef.sum()/len(bef) 
+            v_g = aft.sum()/len(aft)
+        else:
+            v_e = 0
+            v_g = 0
         ve_si = v_e*30/8*0.05E-03
         vg_si = v_g*30/8*0.05E-03
         k = 3.39249E-08
@@ -36,15 +48,29 @@ def returnFinal(voltage):
         final.append({'particle': i, 'turning_point': j, 'v_e': v_e, 'v_g': v_g, 'n': n})
     return final
 
+def highestNumber(start, end, number, all_angles):
+
+    turning_dict = dict()
+    intervals = np.linspace(start, end, number)
+    step = (end-start)/number
+    for i in intervals:
+        N = len([x for x in all_angles if x > i and x < i+step])
+        mean = int(i + step/2)
+        turning_dict[str(mean)] = N
+    key = int(max(turning_dict.items(), key=operator.itemgetter(1))[0])
+    return key
+
 directory = os.fsencode("csv")
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    df = pd.DataFrame.from_csv(str(filename))
+    df = pd.DataFrame.from_csv("csv/"+str(filename))
     pivoted = df.pivot(index = 'frame', columns = 'particle')
     pivoted_y = pivoted['y']
     pivoted_dy = pivoted['dy']
-    filtered_particles, turning_indices = returnParticles()
-    final = returnFinal(int(filename[:3]))
+    turningIndices = getIndices()
+    middle = highestNumber(pivoted_dy.index[0], pivoted_dy.index[-1], 50, turningIndices)
+    filtered_particles, turning_indices = returnParticles(middle)
+    final = returnFinal(int(filename.split('-')[0]))
     data_final = pd.DataFrame(final)
-    data_final.to_csv(str(filename))
+    data_final.to_csv("final_csv/final_"+str(filename))
